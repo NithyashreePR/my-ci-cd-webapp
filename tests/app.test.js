adef8983b7849fec45beee1ad479b04ab87eabfb
NithyashreePR <nithyashreepr10@gmail.com>
2025-05-29 13:56:05 +0530

    final

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("../index");
 
 describe("GET /api/health", () => {
   it("should return status 200 and server status", async () => {
